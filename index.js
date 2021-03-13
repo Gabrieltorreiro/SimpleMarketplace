97f1812779f6378af161255822ae1a34e6a0862e
Gabriel <gabriellopesbusiness@gmail.com>
2021-03-13 16:31:46 -0300

    Make simple as possible and insert mysql database

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'admin',
+    password: '123456',
+    database: 'marketplace'
+});
+
+connection.connect((err) => {
+    console.error(err, "mysql error");
+});
 
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.render('pages/index');
-});
-
-app.get('/client/login', (req, res) => {
-    res.render('pages/client/login');
-});
-
-app.get('/seller/login', (req, res) => {
-    res.render('pages/seller/login');
+app.get('/product-list.html', (req, res) => {
+    connection.query('SELECT * FROM product;', (err, products) => {
+        res.render('product-list',{products: products});
+    });
 });
 
 app.disable('x-powered-by');
